1511a11d2c1a4adce70109ff153dc80a54225f04
Marilyn <speccykitsune@gmail.com>
2025-08-18 00:07:40 -0500

    Update alphabet.lua

--- Lua/alphabet.lua
+++ Lua/alphabet.lua
@@ -4,8 +4,9 @@
 local alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 for i=0, string.len(alphabet)-1 do
-  rawset(_G, alphabet[i+1], i)
-  rawset(_G, string.lower(alphabet[i+1]), i+36)
+  local letter = string.sub(alphabet, i+1, i+1)
+  rawset(_G, letter, i)
+  rawset(_G, string.lower(letter), i+36)
 end
 
 -- Now you know your ABCs, next time won't you rawset with me?
